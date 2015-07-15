b1ed072f79946173418ba954003d1513277e15df
ryanfourier <ryan.hammang@gmail.com>
2015-07-15 10:29:30 -0600

    sync. I don't know where I'm at.

--- code.js
+++ code.js
@@ -56,3 +56,12 @@ else
 {
     console.log("I'll keep practicing coding and racing.")
 }
+
+// Parameter is a number, and we do math with that parameter
+var timesTwo = function(number) {
+    return number * 2;
+};
+
+// Call timesTwo here!
+var newNumber = timesTwo(3)
+console.log(newNumber);
